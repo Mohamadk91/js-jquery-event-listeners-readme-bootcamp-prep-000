845b0ebe0f416458ddd19bb9f0109b3c4d1b1234
Mohamad Khayat <moek91@outlook.com>
2017-12-29 17:10:30 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,39 +1,39 @@
 function getIt(){
   $("p").on("click", function() {
-     alert('Hey!');
-     return;
-   })
- }
+    alert('Hey!');
+    return;
+  })
+}
 
- function frameIt(){
-   $("img").on("load", function(){
-     $('img').addClass('tasty')
-     return;
-   })
- }
+function frameIt(){
+  $("img").on("load", function(){
+    $('img').addClass('tasty')
+    return;
+  })
+}
 
- function pressIt(){
-   $('form').on('keydown', function(key){
-     if(key.which == 71){
-       alert('you pressed g');
+function pressIt(){
+  $('form').on('keydown', function(key){
+    if(key.which == 71){
+      alert('you pressed g');
 
-     }
-   })
- }
+    }
+  })
+}
 
- function submitIt(){
+function submitIt(){
 
-   $('form').on('submit', function(){
-     alert('Your form is going to be submitted now.');
-   })
- }
+  $('form').on('submit', function(){
+    alert('Your form is going to be submitted now.');
+  })
+}
 
 
- $(document).ready(function(){
-    getIt();
-    frameIt();
-    pressIt();
-    submitIt();
-  });}
-function
+$(document).ready(function(){
+   getIt();
+   frameIt();
+   pressIt();
+   submitIt();
+
+
 });
